b1be1cf1f74ad6685658d36f58eaa74994e5581f
m.pluciennik <m.pluciennik@komputronik-biznes.pl>
2019-05-27 16:54:16 +0200

    Connecting BookItem as a Child Component of BookList

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,16 +1,19 @@
-
 <template>
   <div>
     <h1>{{ title }}</h1>
     <ul>
-      <li v-for="book in books">{{ book.title }}:{{ book.author }}</li>
+      <book-item v-for="book in books" :book="book" :key="book"></book-item>
     </ul>
   </div>
 </template>
 
 <script>
+import BookItem from "./BookItem";
 export default {
   name: "BookList",
+  components: {
+    BookItem
+  },
   data() {
     return {
       title: "All Books",
